6a6572316de0a1e556121e34c98ce0761eb28ff4
dan-howd <10663987+dan-howd@users.noreply.github.com>
2016-08-27 16:22:12 -0600

    Remove some hacky code I found via google search
    The code was supposed to eliminate the need to run `rake
    railties:migrations:install`, but I decided that was a bit hacky and
    removed the code.

--- lib/blog/engine.rb
+++ lib/blog/engine.rb
@@ -1,11 +1,5 @@
 module Blog
   class Engine < ::Rails::Engine
     isolate_namespace Blog
-
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        app.config.paths["db/migrate"].concat(config.paths["db/migrate"].expanded)
-      end
-    end
   end
 end
