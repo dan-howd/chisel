9728d30a9c012c55deefcf73a018ee29a063ca44
dan-howd <10663987+dan-howd@users.noreply.github.com>
2016-11-25 16:07:24 -0700

    Remove authenticate_user filters
    Authentication should be out of the scope of this engine, and left to
    the host application.

--- app/controllers/blog/admin/articles_controller.rb
+++ app/controllers/blog/admin/articles_controller.rb
@@ -2,7 +2,6 @@ require_dependency "blog/application_controller"
 
 module Blog
   class Admin::ArticlesController < Admin::ApplicationController
-    before_filter :authenticate_user!
     before_action :set_admin_article, only: [:show, :edit, :update, :destroy]
 
     # GET /admin/articles
