85aabd42284a273debe0dd0b763b5a86d0c5551d
dan-howd <10663987+dan-howd@users.noreply.github.com>
2016-11-25 16:10:32 -0700

    Add some libs: pry, kaminari, sass-rails
    Adds sass-rails since it was needed to run tests, and pry for
    development/debugging purposes.

--- lib/blog/engine.rb
+++ lib/blog/engine.rb
@@ -1,5 +1,12 @@
 module Blog
   class Engine < ::Rails::Engine
     isolate_namespace Blog
+
+    require 'rubygems'
+    require 'kaminari'
+
+    if !Rails.env.production?
+      require 'pry'
+    end
   end
 end
