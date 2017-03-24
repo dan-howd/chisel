f091d3db6f24bd6d5a6f69185ac94f1ff9f38a70
dan-howd <10663987+dan-howd@users.noreply.github.com>
2017-03-24 17:47:43 -0600

    Require authentication for admin related actions

--- app/controllers/blog/application_controller.rb
+++ app/controllers/blog/application_controller.rb
@@ -2,5 +2,10 @@ module Blog
   class ApplicationController < ActionController::Base
     layout "application"
     protect_from_forgery with: :exception
+
+    if !Rails.env.test?
+      before_filter :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
+    end
+
   end
 end
